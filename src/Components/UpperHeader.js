32a0157426b9157b34c1d38f7f262cc84c86beda
ppanwar <ppanwar@cholachariots.com>
2024-03-18 09:33:38 +0530

    new update

--- src/Components/UpperHeader.js
+++ src/Components/UpperHeader.js
@@ -4,8 +4,8 @@ const UpperHeader = () => {
   return (
     <div className="flex justify-between items-center
      w-full   h-10 bg-[#282828] text-white font-sans">
-      <span className="lg:ml-4    ">Call Us : +91 50016223</span>
-      <span className="mx-4 ">Email: info@vitpark.com</span>
+      <span className="lg:ml-4 ml-3    ">Call Us : +91 50016223</span>
+      <span className="lg:mx-6  mr-2  ">Email: info@vitpark.com</span>
     </div>
   )
 };
